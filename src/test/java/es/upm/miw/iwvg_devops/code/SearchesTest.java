6d131c6cf03fb2d3146b01566d37bee77b599740
David Bausela <david.bausela@alumnos.upm.es>
2022-10-04 06:13:13 +0200

    added search c (find 1) to Searches and SearchesTest #11

---
+++ src/test/java/es/upm/miw/iwvg_devops/code/SearchesTest.java
@@ -0,0 +1,17 @@
+package es.upm.miw.iwvg_devops.code;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SearchesTest {
+
+    @Test
+    void testFindDecimalFractionByUserName() {
+        assertEquals(List.of(1.0, 1.0, Double.NaN, Double.NaN, 1.0),
+                new Searches().findDecimalFractionByUserName("Paula").collect(Collectors.toList()));
+    }
+}
